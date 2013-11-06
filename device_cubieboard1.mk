9b5fda5c0e3b1813651c210ae84ab0797752cfee
Ji Yonggang <jungleji@gmail.com>
2013-11-06 16:13:05 +0800

    Add IR key layout

--- device_cubieboard1.mk
+++ device_cubieboard1.mk
@@ -17,6 +17,9 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+PRODUCT_COPY_FILES += \
+    device/softwinner/jungle-cubieboard/sun4i-ir.kl:system/usr/keylayout/sun4i-ir.kl
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
